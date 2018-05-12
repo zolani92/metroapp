76475817a3b876fe7080cc28d4f304b7ebcb716c
rlam <rlam@egencia.com>
2018-05-12 18:08:35 +0200

    Remove api

--- api/server/db/mongoose.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/MetroApp', {'useMongoClient': true});
-mongoose.Promise = global.Promise;
-
-module.exports = { mongoose };
